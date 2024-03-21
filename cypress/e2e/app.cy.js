d77b9cb870d6236efc99fe49dc2bec0f17ef18f5
CopyDemon <shengdevelop@gmail.com>
2024-03-21 15:45:12 -0700

    roll back to wait to check why pytest is broken

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,5 +1,8 @@
 describe('loads the app UI correctly', () => {
+	const waitTImes = 3000;
 	const retryTimes = 3;
+	// { retries: retryTimes }
+
 	// base on webapp url "baseUrl" visit webapp
 	beforeEach(() =>{
 		cy.visit(Cypress.config('baseUrl'));
@@ -11,18 +14,23 @@ describe('loads the app UI correctly', () => {
 		cy.log(`App is up and running at ${Cypress.config('baseUrl')}`);
 	})
 
+	// Cypress wait frontend app to fetch data and rerender UI
+	it('give cypress some time to wait for ui to fetch data and rerender', ()=>{
+		cy.wait(waitTImes)
+	})
+
 	//check if header component exists
-	it('check app #header exists', { retries: retryTimes }, () => {
+	it('check app #header exists',() => {
 		cy.get('#header').should('exist');
 	})
 
 	//check flowsheet component exists
-	it('check app #flowsheet-wrapper exists', { retries: retryTimes }, () => {
+	it('check app #flowsheet-wrapper exists', () => {
 		cy.get('#flowsheet-wrapper').should('exist');
 	})
 
 	//check stream table component exists
-	it('check app #stream-table exist', { retries: retryTimes }, () => {
+	it('check app #stream-table exist', () => {
 		cy.get('#stream-table').should('exist');
 	})
 
